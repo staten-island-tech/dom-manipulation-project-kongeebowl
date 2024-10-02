8fc959e7cd135663fd0c915a165ffd25706f6b94
kongeebowl <kadenp11@nycstudents.net>
2024-10-02 11:37:18 -0400

    y

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const DOMSelectors = {};
+const DOMSelectors = {
+  header: document.querySelector("h1"),
+  description: document.querySelector(".card-desc"),
+  items: document.querySelectorAll("li"),
+};
+console.log(DOMSelectors.description);
+
+function formThing() {
+  //defines the function
+  let form = document.querySelector("form");
+  //listen for a click event
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    //logging the click event
+    console.log(event.target);
+    //get infomation from the form
+  });
+}
+//calls the function
+formThing();
