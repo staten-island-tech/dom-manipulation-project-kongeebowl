ec72cf73cb63d0f8ee4e1d8b289b3dd932a7a99a
kongeebowl1 <kadenpang09@gmail.com>
2024-10-10 22:57:36 -0400

    I DID IT

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const DOMSelectors = {
-  header: document.querySelector("h1"),
-  items: document.querySelectorAll("li"),
-  image: document.getElementById("card-img"),
-  card: document.getElementById("card"),
-  description: document.getElementById("card-desc"),
+  button: document.getElementById("btn"),
+  box: document.getElementById("container"),
+  titleInput: document.getElementById("input-title"),
+  imageInput: document.getElementById("input-img"),
+  descInput: document.getElementById("input-desc"),
 };
 console.log(DOMSelectors.description);
 
-function formThing() {
-  //defines the function
-  let form = document.querySelector("form");
-  //listen for a click event
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    //logging the click event
-    console.log(event.target);
-    //get infomation from the form
-  });
-}
-//calls the function
-formThing();
+DOMSelectors.button.addEventListener("click", function () {
+  let title = DOMSelectors.titleInput.value;
+  let image = DOMSelectors.imageInput.value;
+  let description = DOMSelectors.descInput.value;
+
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<div class="card"> 
+        <h3>${title}</h3>
+        <img src="${image}" alt="Card Image" class="card-img">
+        <p>${description}</p>
+      </div>
+      `
+  );
+  DOMSelectors.titleInput.value = "";
+  DOMSelectors.imageInput.value = "";
+  DOMSelectors.descInput.value = "";
+});
